22c651525fc7da1fdbc10139718439a0d39c31f5
Brendan Greene <brendanjgreene@gmail.com>
2017-04-16 00:39:36 +0100

    stylingimprovements

--- hello/forms.py
+++ hello/forms.py
@@ -2,11 +2,12 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    contact_name = forms.CharField(required=True)
-    contact_email = forms.EmailField(required=True)
+    contact_name = forms.CharField(required=True, label="Your Name")
+    contact_email = forms.EmailField(required=True, label="Your Email")
     content = forms.CharField(
         required=True,
-        widget=forms.Textarea
+        widget=forms.Textarea,
+        label="What You want Us To know!"
     )
 
 # https://hellowebapp.com/news/tutorial-setting-up-a-contact-form-with-django/
