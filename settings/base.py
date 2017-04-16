d94718cdbe4fdbee09b98e3dfa8040e559173940
Brendan Greene <brendanjgreene@gmail.com>
2017-04-16 10:55:42 +0100

    wsgi.py

--- settings/base.py
+++ settings/base.py
@@ -97,7 +97,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '')
+STATIC_ROOT = ''
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),  # static directory at the project level
