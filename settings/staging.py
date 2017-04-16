a64d09039ddc258929a515dd280b18abd7087efb
Brendan Greene <brendanjgreene@gmail.com>
2017-04-16 04:03:34 +0100

    working on database

--- settings/staging.py
+++ settings/staging.py
@@ -10,7 +10,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(
+DATABASES['default'] = dj_database_url.parse(
     "mysql://b303b6a76a6c1a:a0cce2e7@eu-cdbr-west-01.cleardb.com/heroku_42692666c95cfac?reconnect=true")
 
 # Stripe environment variables
